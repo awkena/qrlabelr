6b8f5221d9ac2366314ede6bb148a4c549bb279e
Alex Kena <alex.kena24@gmail.com>
2023-04-04 15:56:26 -0600

    test for Treetag label

--- tests/testthat/test-create_label.R
+++ tests/testthat/test-create_label.R
@@ -37,6 +37,46 @@ test_that("Create plot label PDF successful generation", {
   
 })
 
+
+test_that("Create Treetag plot label PDF successful generation", {
+  # skip_on_cran()
+  path <- tempdir()
+  setwd(path)
+  expect_invisible(
+    create_label(wdt = 1, 
+                 hgt = 6, 
+                 page_wdt = 4, 
+                 page_hgt = 6, 
+                 top_mar = 0, 
+                 bot_mar = 0, 
+                 left_mar = 0, 
+                 right_mar = 0, 
+                 numrow = 1L, 
+                 numcol = 4L, 
+                 filename = 'PlotLabel', 
+                 font_sz = 10,
+                 Treetag = TRUE,
+                 family = 'sans', 
+                 rounded = FALSE, 
+                 top_left_1 = paste("Plot:", 101:110), 
+                 top_left_2 = paste("Row:", c(rep(1, 6), rep(2, 4))), 
+                 top_right_1 = paste("Rep:", rep(1, 10)), 
+                 top_right_2 = paste("Col:", c(1:6, 1:4)), 
+                 center_right_1 = paste("iBLOCK:", c(rep(1, 6), rep(2, 4))), 
+                 center_right_2 = paste("Seed:", rep("OFF_NUR", 10)), 
+                 center_right_3 = rep("AWk", 10), 
+                 bottom_left_1 = paste("Loc:", rep("BAMBEY", 10)), 
+                 bottom_left_2 = paste0("G-", 1:10),
+                 unique_id = paste("KUMASI2023_PYT", c(101:110), 
+                                   c(rep(1, 6), rep(2, 4)), c(1:6, 1:4),
+                                   sep = "_") ))
+  expect_true(length(list.files(path = ".", pattern = "\\.pdf$")) > 0)
+  on.exit(unlink(path))
+  # expect_true(length(list.files(path = ".", pattern = "\\.csv$")) > 0)
+  
+})
+
+
 test_that("Create plot label input errors", {
 
   expect_error(create_label(wdt = 2, 
