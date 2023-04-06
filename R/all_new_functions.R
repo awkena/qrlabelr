29f1f2f01dd042310f3662ef1f015a033d60fea4
Alex Kena <alex.kena24@gmail.com>
2023-04-06 14:07:28 -0600

    Examples for functions updated

--- R/all_new_functions.R
+++ R/all_new_functions.R
@@ -12,7 +12,7 @@
 #' @examples
 #'
 #' \dontrun{
-#' make_qrcode("PLT101-NURSERY", 4)
+#' make_qrcode("KUMASI2023_PYT_101_1_1", ec_level = 3)
 #' }
 #'
 #'
@@ -70,6 +70,76 @@ make_qrcode <- function(my_id, ec_level = 3){
 #' @param unique_id is a vector containing unique identifiers or strings to generate QR codes.
 #' @param ec_level error correction level (`0` - `3`, lowest to highest)
 #' @param ... additional optional arguments to be supplied.
+#' 
+#' @examples
+#'
+#' \dontrun{
+#' Create rectangular plot labels based on the Avery 94220 template
+#' library(qrlabelr)
+#' create_label(
+#' wdt = 2, 
+#' hgt = 1, 
+#' page_wdt = 8.5, 
+#' page_hgt = 11, 
+#' top_mar = 0.625, 
+#' bot_mar = 0.625, 
+#' left_mar = 0.625, 
+#' right_mar = 0.625, 
+#' numrow = 8L, 
+#' numcol = 3L, 
+#' filename = 'PlotLabel', 
+#' font_sz = 10,
+#' Treetag = FALSE,
+#' family = 'sans', 
+#' rounded = TRUE, 
+#' top_left_1 = paste("Plot:", 101:110), 
+#' top_left_2 = paste("Row:", c(rep(1, 6), rep(2, 4))), 
+#' top_right_1 = paste("Rep:", rep(1, 10)), 
+#' top_right_2 = paste("Col:", c(1:6, 1:4)), 
+#' center_right_1 = paste("iBLOCK:", c(rep(1, 6), rep(2, 4))), 
+#' center_right_2 = paste("Seed:", rep("OFF_NUR", 10)), 
+#' center_right_3 = rep("AWK", 10), 
+#' bottom_left_1 = paste("Loc:", rep("BAMBEY", 10)), 
+#' bottom_left_2 = paste0("G-", 1:10),
+#' unique_id = paste("KUMASI2023_PYT", c(101:110), 
+#'                   c(rep(1, 6), rep(2, 4)), c(1:6, 1:4),
+#'                   sep = "_"), 
+#' ec_level = 3)
+#'  }
+#'  
+#' \dontrun{
+#' Create treetag plot labels
+#' library(qrlabelr)
+#' create_label(
+#' wdt = 1, 
+#' hgt = 6, 
+#' page_wdt = 4, 
+#' page_hgt = 6, 
+#' top_mar = 0, 
+#' bot_mar = 0, 
+#' left_mar = 0, 
+#' right_mar = 0, 
+#' numrow = 1L, 
+#' numcol = 4L, 
+#' filename = 'PlotLabel', 
+#' font_sz = 10,
+#' Treetag = TRUE,
+#' family = 'sans', 
+#' rounded = FALSE, 
+#' top_left_1 = paste("Plot:", 101:110), 
+#' top_left_2 = paste("Row:", c(rep(1, 6), rep(2, 4))), 
+#' top_right_1 = paste("Rep:", rep(1, 10)), 
+#' top_right_2 = paste("Col:", c(1:6, 1:4)), 
+#' center_right_1 = paste("iBLOCK:", c(rep(1, 6), rep(2, 4))), 
+#' center_right_2 = paste("Seed:", rep("OFF_NUR", 10)), 
+#' center_right_3 = rep("AWK", 10), 
+#' bottom_left_1 = paste("Loc:", rep("BAMBEY", 10)), 
+#' bottom_left_2 = paste0("G-", 1:10),
+#' unique_id = paste("KUMASI2023_PYT", c(101:110), 
+#'                   c(rep(1, 6), rep(2, 4)), c(1:6, 1:4),
+#'                   sep = "_"),
+#' ec_level = 3)
+#'  }
 #'
 #' @export
 create_label <- function(
@@ -969,7 +1039,7 @@ field_label <- function(dat,
 #' center_right_id1 = 'IBLOCK',
 #' center_right_id2 = 'SEED_SOURCE',
 #' bottom_left_id1 = 'LOCATION',
-#' bottom_left_id2 = 'TREATMENT',
+#' bottom_left_id2 = 'TREATMENT'
 #' )
 #' }
 #'
