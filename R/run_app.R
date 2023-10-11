a9150b1f491ce3677c4e35f4b958ff47bd876219
Alexander Wireko Kena <alex.kena24@gmail.com>
2023-10-11 12:07:12 +0000

    Example added to run_app

--- R/run_app.R
+++ R/run_app.R
@@ -3,6 +3,11 @@
 #' @description An interface function to launch the 'EasyQrlabelr' 'shiny' app.
 #' @returns No return value, called for side effects. Opens the 'EasyQrlabelr' 'shiny' app in your default browser.
 #'
+#' @examples
+#' library(qrlabelr)
+#' if(interactive())
+#' qrlabelr::run_app()
+#' 
 #' @concept "plot labels"
 #'
 #' @export
